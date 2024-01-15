2175556cff517ce2a63c57f90a4c45aee17ea797
Heili Lowman <heili.lowman@gmail.com>
2024-01-15 09:57:16 -0800

    Ran SAM model for 2 years back.

--- R/SAM_STAN_script.R
+++ R/SAM_STAN_script.R
@@ -74,7 +74,7 @@ mcmc_areas(stan_test_run,
     subtitle = "with medians and 95% intervals"
   )
 
-#### SAM Model Fit ####
+#### SAM Model Fit 12mo ####
 
 # From Ana's JAGS script, need to make the necessary data transformations.
 # And going to replace NAs with mean values for the time being.
@@ -216,5 +216,138 @@ mcmc_intervals(stan_sam_run,
     title = "Posterior distributions",
     subtitle = "with medians and 95% intervals"
   )
+
+#### SAM Model Fit 24mo ####
+
+# Re-running much of the same code from the chunk above so these can
+# stand on their own.
+
+# From Ana's JAGS script, need to make the necessary data transformations.
+# And going to replace NAs with mean values for the time being.
+data_noNA_sam <- data %>%
+  # replace NAs with mean
+  mutate(CN_gapfilled = replace_na(mean_CN, mean(mean_CN, na.rm = TRUE))) %>%
+  mutate(Temp_gapfilled = replace_na(mean_Temp_C, mean(mean_Temp_C, na.rm = TRUE)))
+
+# OPTION 2: Replace w/ values from normal distribution outside the model script,
+# as Ana did in her model script. rnorm(mu, sigma)
+# Could also consider differences among sites/within years.
+cn_dist <- rnorm(100, 
+                 mean(data$mean_CN, na.rm = TRUE),
+                 sd(data$mean_CN, na.rm = TRUE))
+
+temp_dist <- rnorm(100,
+                   mean(data$mean_Temp_C, na.rm = TRUE),
+                   sd(data$mean_CN, na.rm = TRUE))
+
+data_repNA_sam <- data %>%
+  # replace NAs with randomly generated values from normal dist.
+  mutate(CN_gapfilled = replace_na(mean_CN, sample(cn_dist, 1))) %>%
+  mutate(Temp_gapfilled = replace_na(mean_Temp_C, sample(temp_dist,1)))
+
+# Create new dataset of lags using gapfilled data.
+Temp_lags_sam <- data_repNA_sam %>%
+  dplyr::select(SITE, year, month, Temp_gapfilled) %>%
+  group_by(SITE) %>%
+  arrange(SITE, year, month) %>%
+  #this creates a column for every lag 1:12 months ago
+  do(data.frame(., setNames(shift(.$Temp_gapfilled, 1:24), c("mean_Temp_Cl1",
+                                                             "mean_Temp_Cl2",
+                                                             "mean_Temp_Cl3",
+                                                             "mean_Temp_Cl4",
+                                                             "mean_Temp_Cl5",
+                                                             "mean_Temp_Cl6",
+                                                             "mean_Temp_Cl7",
+                                                             "mean_Temp_Cl8",
+                                                             "mean_Temp_Cl9",
+                                                             "mean_Temp_Cl10",
+                                                             "mean_Temp_Cl11",
+                                                             "mean_Temp_Cl12",
+                                                             "mean_Temp_Cl13",
+                                                             "mean_Temp_Cl14",
+                                                             "mean_Temp_Cl15",
+                                                             "mean_Temp_Cl16",
+                                                             "mean_Temp_Cl17",
+                                                             "mean_Temp_Cl18",
+                                                             "mean_Temp_Cl19",
+                                                             "mean_Temp_Cl20",
+                                                             "mean_Temp_Cl21",
+                                                             "mean_Temp_Cl22",
+                                                             "mean_Temp_Cl23",
+                                                             "mean_Temp_Cl24")))) %>%
+  ungroup()
+
+#join back together so each observation has its lags
+data3_sam <- data_repNA_sam %>%
+  left_join(Temp_lags_sam, by = c("SITE", "year", "month",
+                                  "Temp_gapfilled"))  
+
+saveRDS(data3_sam, here("dat_working", "SAM_structured_input_data_2ylag_STAN.RDS"))
+
+# Assemble data for model
+
+# Response data
+
+# base dataset creation
+# will make it easier to omit first two years of data from CN and Temp data
+data3_sam_scaled <- data3_sam %>%
+  pivot_longer(Temp_gapfilled:mean_Temp_Cl24,
+               names_to = "lag",
+               values_to = "temp") %>%
+  # scale all temps to each other
+  mutate(temp = scale(temp)) %>%
+  pivot_wider(names_from = lag,
+              values_from = temp) %>%
+  # remove first two years of data from all three sites for which
+  # we do not have proper lagged temperature data available for now
+  drop_na(mean_Temp_Cl1:mean_Temp_Cl24)
+
+# nutritional content (C:N) data
+y <- as.vector(log(data3_sam_scaled$CN_gapfilled))
+
+# temperature lag matrix 
+Temp <- data3_sam_scaled %>%
+  dplyr::select(Temp_gapfilled:mean_Temp_Cl24) %>%
+  #make matrix
+  as.matrix()
+
+# Loop indices
+# number of observations
+nrows <- nrow(data3_sam_scaled)
+# number of months included in each antecedent summation
+nlag <- 25
+
+# Prep data for STAN as a list
+data_stan_sam <- list(
+  #indices for loops
+  nrows = nrows,
+  nlag = nlag,
+  #reponse data
+  CN = y,
+  #temperature data
+  Temp = Temp,
+  #prior
+  alpha = c(rep(1/25, 25))) # prior is 0.04
+
+saveRDS(data_stan_sam, here("model_summaries", "STAN_input_list_2ylag.RDS"))
+
+# Fit SAM model.
+stan_sam_run_2y <- stan(file = "models/SAM_STAN_template.stan",
+                     data = data_stan_sam,
+                     chains = 3,
+                     iter = 5000,
+                     control = list(max_treedepth = 12))
+
+# Ran on Pinyon, started at 10:33AMPM, finished 
+# 1823 divergences, eek!
+
+saveRDS(stan_sam_run_2y, here("model_summaries", "STAN_output_list_011524.RDS"))
+
+# Quick plot of model values.
+plot(stan_sam_run_2y, pars = c("wA[1]", "wA[2]", "wA[3]", "wA[4]", "wA[5]", "wA[6]", 
+                            "wA[7]", "wA[8]", "wA[9]", "wA[10]", "wA[11]", "wA[12]",
+                            "wA[13]", "wA[14]", "wA[15]", "wA[16]", 
+                            "wA[17]", "wA[18]", "wA[19]", "wA[20]", "wA[21]", 
+                            "wA[22]", "wA[23]", "wA[24]", "wA[25]"))
 
 # End of script.
